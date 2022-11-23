4f3bbe58ec6487baabfd74b285c0964840ef5c07
Kyle Mulleady <kyle@windwardapps.com>
2022-11-23 11:40:38 -1000

    trends

--- src/services/trends.ts
+++ src/services/trends.ts
@@ -3,22 +3,33 @@ import db from '../lib/db'
 
 // eslint-disable-next-line quotes
 const CREATED_DATE_AS_INT = `cast(replace(substr("createdAt", 0, 11), '-', '') as integer)`
+const MIN_COUNT = 3
 
 export async function getTrendingSearches(range: ManipulateType = 'month') {
   const start = Number(dayjs().subtract(1, range).format('YYYYMMDD'))
   const query = `select * from "user_activity" where "event" = 'Search' and ${CREATED_DATE_AS_INT} >= ${start}`
   const rows = (await db.query(query)) as any[]
-  const searches = new Set()
+  const searchTextToCount: any = {}
   for (const row of rows) {
     try {
       row.data = JSON.parse(row.data as string)
       if (row.data.terms && !row.data.taxonomies) {
-        searches.add(row.data.terms.toLowerCase().trim())
+        const text = row.data.terms.toLowerCase().trim()
+        if (!searchTextToCount[text]) {
+          searchTextToCount[text] = 0
+        }
+        searchTextToCount[text]++
       }
     } catch (err) {
       // no op
     }
   }
 
-  return [...searches]
+  const rv = []
+  for (const [text, count] of Object.entries(searchTextToCount)) {
+    if ((count as number) >= MIN_COUNT) {
+      rv.push(text)
+    }
+  }
+  return rv
 }
