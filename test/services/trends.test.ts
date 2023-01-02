3ca14404390e97181e4ec0b4cb1a9838d2d368b1
Kyle Mulleady <kyle@windwardapps.com>
2023-01-02 09:50:07 -1000

    tests

--- test/services/trends.test.ts
+++ test/services/trends.test.ts
@@ -1,10 +1,8 @@
 import prisma from '../../src/lib/prisma'
 import { getRelatedSearches } from '../../src/services/trends'
 
-describe('services/trends', () => {
-  test('getRelatedSearches()', async () => {
-    const ua = await prisma.user_activity.findFirst({ rejectOnNotFound: true })
-    const result = await getRelatedSearches('food', ua.userId)
-    expect(result).toBeInstanceOf(Array)
-  })
+test('getRelatedSearches()', async () => {
+  const ua = await prisma.user_activity.findFirst({ rejectOnNotFound: true })
+  const result = await getRelatedSearches('food', ua.userId)
+  expect(result).toBeInstanceOf(Array)
 })
