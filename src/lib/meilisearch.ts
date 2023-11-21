b4dbdf5f8f04344e17a4de6309eaddd2c963857e
Kyle Mulleady <kmulleady@hawaiidata.org>
2023-11-20 15:07:57 -1000

    MEILISEARCH_KEY

--- src/lib/meilisearch.ts
+++ src/lib/meilisearch.ts
@@ -1,6 +1,8 @@
 import { MeiliSearch } from 'meilisearch'
 
 const host = process.env.MEILISEARCH_HOST || 'http://localhost:7700'
-const meilisearch = new MeiliSearch({ host })
+const apiKey = process.env.MEILISEARCH_KEY
+
+const meilisearch = new MeiliSearch({ host, apiKey })
 
 export default meilisearch
