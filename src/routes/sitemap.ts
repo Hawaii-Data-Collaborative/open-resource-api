4bb6cc3dc968eb2c23b0f3a0484341b95c7399b3
Kyle Mulleady <kmulleady@hawaiidata.org>
2024-09-19 15:34:37 -1000

    sitemap

--- src/routes/sitemap.ts
+++ src/routes/sitemap.ts
@@ -9,43 +9,32 @@ const router = new Router({
   prefix: `${BASE_PREFIX}/sitemap`
 })
 
-function isGooglebot(ctx) {
-  return ctx.get('User-Agent').toLowerCase().includes('googlebot')
-}
-
-function applyBotInfo(ctx) {
-  const isBot = isGooglebot(ctx)
-  if (isBot) {
-    ctx.set('Content-Security-Policy', "script-src 'self' 'unsafe-inline'")
-  }
-}
-
 router.get('/', async (ctx) => {
-  const isBot = isGooglebot(ctx)
-  const html = nunjucks.render('home.html', { redirect: isBot })
-  applyBotInfo(ctx)
+  const html = nunjucks.render('home.html')
   ctx.body = html
 })
 
 router.get('/about', async (ctx) => {
-  const isBot = isGooglebot(ctx)
-  const html = nunjucks.render('about.html', { redirect: isBot })
-  applyBotInfo(ctx)
+  const html = nunjucks.render('about.html')
   ctx.body = html
 })
 
+let cachedPrograms
 router.get('/programs', async (ctx) => {
-  const programs = await prisma.program.findMany({ where: { Status__c: { not: 'Inactive' } } })
-  const programIds = programs.map((p) => p.id)
-  const sitePrograms = await prisma.site_program.findMany({
-    select: { id: true, Site__c: true, Program__c: true },
-    where: { Program__c: { in: programIds } }
-  })
-  const results = await buildResults(sitePrograms as any, programs)
-  results.sort((a, b) => a.service_name.localeCompare(b.service_name))
-  const isBot = isGooglebot(ctx)
-  const html = nunjucks.render('programs.html', { redirect: isBot, programs: results })
-  applyBotInfo(ctx)
+  if (!cachedPrograms) {
+    const programs = await prisma.program.findMany({ where: { Status__c: { not: 'Inactive' } } })
+    const programIds = programs.map((p) => p.id)
+    const sitePrograms = await prisma.site_program.findMany({
+      select: { id: true, Site__c: true, Program__c: true },
+      where: { Program__c: { in: programIds } }
+    })
+    cachedPrograms = await buildResults(sitePrograms as any, programs)
+    cachedPrograms.sort((a, b) => a.service_name.localeCompare(b.service_name))
+    setTimeout(() => {
+      cachedPrograms = null
+    }, 1000 * 60 * 10)
+  }
+  const html = nunjucks.render('programs.html', { programs: cachedPrograms })
   ctx.body = html
 })
 
@@ -57,9 +46,7 @@ router.get('/programs/:id', async (ctx) => {
     program = null
   }
 
-  const isBot = isGooglebot(ctx)
-  const html = nunjucks.render('program.html', { redirect: isBot, program })
-  applyBotInfo(ctx)
+  const html = nunjucks.render('program.html', { program })
   ctx.body = html
 })
 
