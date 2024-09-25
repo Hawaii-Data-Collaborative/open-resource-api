c4104c84ba7b3a4d120c0b651ccb85133f02a987
Kyle Mulleady <kmulleady@hawaiidata.org>
2024-09-24 23:00:32 -1000

    sf sync - fetchSitePrograms

--- src/scripts/copyDataFromSF.ts
+++ src/scripts/copyDataFromSF.ts
@@ -78,6 +78,38 @@ async function fetchProgramServices(programIds: string[]) {
   return allRecords
 }
 
+async function fetchSitePrograms(programIds: string[]) {
+  console.log(`[fetchSitePrograms] programIds=${programIds.join(',')}`)
+  const fieldsObj: any = { ...schema.site_program }
+  delete fieldsObj.id
+  delete fieldsObj.keywords
+  delete fieldsObj.attributes
+  fieldsObj.Id = true
+  const fieldsStr = Object.keys(fieldsObj).join(',')
+  const params = querystring.stringify({
+    q: `select ${fieldsStr} from Site_Program__c where Program__c in ('${programIds.join("','")}')`
+  })
+  const token = await getAccessToken()
+  const headers = { Authorization: `Bearer ${token}` }
+  const res = await fetch(`${BASE_URL}/services/data/v60.0/query?${params}`, { headers })
+  let data = await res.json()
+  if (res.status !== 200) {
+    throw new Error(JSON.stringify(data))
+  }
+  const allRecords = [...data.records]
+  while (!data.done) {
+    const nextUrl = `${BASE_URL}${data.nextRecordsUrl}`
+    const res = await fetch(nextUrl, { headers })
+    data = await res.json()
+    if (res.status !== 200) {
+      throw new Error(JSON.stringify(data))
+    }
+    allRecords.push(...data.records)
+  }
+  console.log('[fetchSitePrograms] allRecords.length=%s', allRecords.length)
+  return allRecords
+}
+
 async function save(tableName, data) {
   const filepath = `data/json/${tableName}.json`
   fs.writeFileSync(filepath, JSON.stringify(data, null, 2))
@@ -92,6 +124,9 @@ export async function copyDataFromSF(sobjectName, modelName) {
     const programIds = data.map((o) => o.Id)
     const data2 = await fetchProgramServices(programIds)
     await save('program_service', data2)
+
+    const data3 = await fetchSitePrograms(programIds)
+    await save('site_program', data3)
   }
 
   return data
