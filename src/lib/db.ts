053312b5e9a8381bb43293c9e30d4332f937260c
Kyle Mulleady <kmulleady@hawaiidata.org>
2025-04-22 16:58:53 -1000

    process translations in meilisearchIngest

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,7 @@ const db = new Database(process.env.DB_FILE as string)
 export default {
   query: (sql: string) => {
     return new Promise((resolve, reject) => {
-      db.all(sql, (err: Error, rows: Object[]) => {
+      db.all(sql, (err: Error, rows: any[]) => {
         if (err) return reject(err)
         resolve(rows)
       })
