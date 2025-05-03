8ad8cb2dd48a61c5bb5d075e20a4f3ff29c1d79f
Kyle Mulleady <kmulleady@hawaiidata.org>
2025-05-02 18:58:52 -1000

    koa-static

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import fs from 'fs'
+import path from 'path'
 import Koa from 'koa'
 import json from 'koa-json'
 import bodyParser from 'koa-bodyparser'
 import helmet from 'koa-helmet'
 import morgan from 'koa-morgan'
+import serve from 'koa-static'
 import cors from '@koa/cors'
 import nunjucks from 'nunjucks'
 import pingRouter from './routes/ping'
@@ -11,12 +13,27 @@ import routerV1 from './routes/v1'
 import sitemap from './routes/sitemap'
 import { errorHandler } from './middleware'
 
+const debug = require('debug')('app:server')
+
 const app = new Koa()
 const PORT = Number(process.env.PORT || '3001')
 const HOSTNAME = process.env.HOSTNAME || 'localhost'
 const ACCESS_LOG = process.env.ACCESS_LOG || `${process.cwd()}/access.log`
 
 nunjucks.configure('templates', { noCache: true })
+
+const FRONTEND_DIR = process.env.FRONTEND_DIR
+if (FRONTEND_DIR) {
+  const dir = path.join(__dirname, FRONTEND_DIR)
+  if (fs.existsSync(dir)) {
+    app.use(serve(dir))
+    debug('serving frontend, dir=%s', dir)
+  } else {
+    throw new Error(`FRONTEND_DIR ${dir} does not exist`)
+  }
+} else {
+  debug('FRONTEND_DIR is empty')
+}
 
 morgan.token('remote-addr', (req) => req.headers['x-real-ip'])
 const stream = fs.createWriteStream(ACCESS_LOG, { flags: 'a' })
